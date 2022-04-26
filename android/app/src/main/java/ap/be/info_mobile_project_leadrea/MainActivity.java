a9fc1aa8cf407088ff85ba9ce3c353e843b5a2c0
Rabin Gurung <rabin.gurung@student.ap.be>
2022-04-26 20:01:03 +0200

    add blank page

---
+++ android/app/src/main/java/ap/be/info_mobile_project_leadrea/MainActivity.java
@@ -0,0 +1,6 @@
+package ap.be.info_mobile_project_leadrea;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/pages/admin_login_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AdminLoginPage extends StatefulWidget {
+  const AdminLoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<AdminLoginPage> createState() => _AdminLoginPageState();
+}
+
+class _AdminLoginPageState extends State<AdminLoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
